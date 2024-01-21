1c602f46eff455f610828744b1f698f668820d6d
Saurabh-Gupta890 <98148196+Saurabh-Gupta890@users.noreply.github.com>
2024-01-21 23:42:53 +0530

    Updates

---
+++ precache-manifest.1f7bbafedf1d35a3c8ccfeacc619b1af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0378d02ed3f95b95d16b5596931ce775",
+    "url": "/Saurabh-Gupta890/NewsTak/index.html"
+  },
+  {
+    "revision": "ecfd1abc7a047cd3761a",
+    "url": "/Saurabh-Gupta890/NewsTak/static/css/main.bff65184.chunk.css"
+  },
+  {
+    "revision": "84c0f111d67aad7c28b9",
+    "url": "/Saurabh-Gupta890/NewsTak/static/js/2.4135a25c.chunk.js"
+  },
+  {
+    "revision": "43b07b59a8799f3362738c8366935dda",
+    "url": "/Saurabh-Gupta890/NewsTak/static/js/2.4135a25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448f05d5589f56d383ad",
+    "url": "/Saurabh-Gupta890/NewsTak/static/js/3.5170f710.chunk.js"
+  },
+  {
+    "revision": "ecfd1abc7a047cd3761a",
+    "url": "/Saurabh-Gupta890/NewsTak/static/js/main.d44dcabc.chunk.js"
+  },
+  {
+    "revision": "b46608472726baf82186",
+    "url": "/Saurabh-Gupta890/NewsTak/static/js/runtime-main.4547074d.js"
+  },
+  {
+    "revision": "d47e58c71a6e6573f1e35fcb2beeee2a",
+    "url": "/Saurabh-Gupta890/NewsTak/static/media/loading.d47e58c7.gif"
+  }
+]);
